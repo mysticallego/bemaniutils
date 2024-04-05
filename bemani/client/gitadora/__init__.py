75fda75e165143d57d64fa6357903a680f005677
wz18207 <ww020823456@163.com>
2024-04-05 17:31:03 +0800

    init.
    TODO: add client verify support.

---
+++ bemani/client/gitadora/__init__.py
@@ -0,0 +1,9 @@
+from bemani.client.gitadora.nextage import GitadoraNextageClient
+from bemani.client.iidx.highvoltage import GitadoraHighVoltageClient
+from bemani.client.iidx.fuzzup import GitadoraFuzzUpClient
+
+__all__ = [
+    "GitadoraNextageClient",
+    "GitadoraHighVoltageClient",
+    "GitadoraFuzzUpClient",
+]
