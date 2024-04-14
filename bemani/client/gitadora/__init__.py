1b5bd3daae37b75fe90e85e4f92e59367adeb67e
wz18207 <ww020823456@163.com>
2024-04-14 18:35:52 +0800

    fix gfdm musiclists.
    optimize response speed.

--- bemani/client/gitadora/__init__.py
+++
@@ -1,9 +0,0 @@
-from bemani.client.gitadora.nextage import GitadoraNextageClient
-from bemani.client.iidx.highvoltage import GitadoraHighVoltageClient
-from bemani.client.iidx.fuzzup import GitadoraFuzzUpClient
-
-__all__ = [
-    "GitadoraNextageClient",
-    "GitadoraHighVoltageClient",
-    "GitadoraFuzzUpClient",
-]
